39b5844e92a3445e60430dd7a24ad6f1ae61d8bb
xiaolin <xxxpo@139.com>
2024-06-24 18:30:07 +0800

    v0.0.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.xxxlin.json.ext"
-version = "0.0.1"
+version = "0.0.19"
 
 val localProperty = Properties()
 localProperty.load(project.rootProject.file("local.properties").inputStream())
